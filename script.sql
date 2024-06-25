00cd0f4fdf4a285c2a65a8fb6d8fcf218c980e42
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-06-25 15:38:12 -0300

    Conte quantos projetos estão na tabela

--- script.sql
+++ script.sql
@@ -55,5 +55,7 @@ SELECT COUNT(*) FROM funcionarios; #Conte o número total de funcionários.
 SELECT * FROM projetos WHERE data_inicio > '2023-01-01'; # Selecione os projetos que começam depois de '2023-01-01'. 
 SELECT * FROM funcionarios ORDER BY idade ASC; # Ordene os funcionários por idade em ordem crescente. 
 SELECT * FROM funcionarios WHERE nome LIKE 'A%'; # Selecione os funcionários cujo nome começa com 'A'.
+SELECT COUNT(*) FROM projetos; # Conte quantos projetos estão na tabela. 
+
 /*ASC – Ordem crescente
 DESC – Ordem descendente*
